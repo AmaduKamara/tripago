91649b76f7b49a23a19c67247b9af6dc8560c552
Amkam <amaduskamara@gmail.com>
2022-02-06 01:11:34 +0000

    Add id in db data and display list of data in TripList

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const TripList = () => {
+  const [trips, setTrips] = useState([]);
+
+  console.log(trips);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/trips")
+      .then((response) => response.json())
+      .then((json) => setTrips(json));
+  }, []);
+
   return (
-    <div>
-      <h2 className="text-4xl font-semibold">Trip List</h2>
+    <div className="container mx-auto text-left">
+      <h2 className="text-4xl font-semibold text-center">Trip List</h2>
+      <ul className="mt-10">
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id} className="p-10 shadow-md mb-6 rounded-md border">
+              <h3 className="text-2xl font-semibold text-gray-600">
+                {trip.title}
+              </h3>
+              <p className="text-teal-400 mt-5 text-xl">{trip.price}</p>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
