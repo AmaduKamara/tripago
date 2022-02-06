e99be7bfec1f6aad0e832bed5e3df5164fb735a6
Amkam <amaduskamara@gmail.com>
2022-02-06 00:47:04 +0000

    Install json-server and create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="py-6 flex items-center container mx-32">
+      <h1>Tripago</h1>
+    </div>
+  );
+};
+
+export default Header;
